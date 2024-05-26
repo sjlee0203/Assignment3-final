7c9f46411278ea4a193e601cfb75c95c4aab5a1d
sjlee0203 <161278631+sjlee0203@users.noreply.github.com>
2024-05-26 17:28:57 +1000

    Delete week10 directory

--- week10/calculator.js
+++
@@ -1,8 +0,0 @@
-function calculate() {
-    const num1 = getNumbers('number1');
-    const num2 = getNumbers('number2');
-    const operator = getOperator();
-    let result;
-    
-    switch (operator)
-}
