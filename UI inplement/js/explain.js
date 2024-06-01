1f42ac8e2b922bcd9d22c97aeab633ce2e843152
sjlee0203 <161278631+sjlee0203@users.noreply.github.com>
2024-06-02 08:00:19 +1000

    Delete UI inplement/js directory

--- UI inplement/js/explain.js
+++
@@ -1,5 +0,0 @@
-const toggleIcon = document.querySelector('.toggle-icon');
-const navbar = document.querySelector('.navbar');
-toggleIcon.addEventListener('click', () => {
-    navbar.classList.toggle('active');
-});
--- UI inplement/js/homescript.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const toggleIcon = document.querySelector('.toggle-icon');
-    const navbar = document.querySelector('.navbar');
-
-    toggleIcon.addEventListener('click', () => {
-        navbar.classList.toggle('active');
-    });
-});
